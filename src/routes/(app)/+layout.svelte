dfb3792bfe794b906a910295776f31ab7d36c2c5
Yellowinq <hig4342@naver.com>
2026-05-28 07:25:55 +0900

    :lipstick: ui(layout): restructure layout to include Header component

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+	import Header from '$lib/components/header.svelte';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Header />
+<div class="container mx-auto mt-4">
+	<main>
+		{@render children()}
+	</main>
+</div>
