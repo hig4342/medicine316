c0b9e1701fac701b6cb4ca04815a28f62ac24e7e
Yellowinq <hig4342@naver.com>
2026-05-27 09:52:18 +0900

    :lipstick: feat(components): add theme switcher component

---
+++ src/lib/components/theme-switcher.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button';
+	import SunIcon from '@lucide/svelte/icons/sun';
+	import MoonIcon from '@lucide/svelte/icons/moon';
+</script>
+
+<Button onclick={toggleMode} variant="outline" size="icon">
+	<SunIcon class="scale-100 rotate-0 transition-all! dark:scale-0 dark:-rotate-90" />
+	<MoonIcon class="absolute scale-0 rotate-90 transition-all! dark:scale-100 dark:rotate-0" />
+	<span class="sr-only">Switch theme</span>
+</Button>
