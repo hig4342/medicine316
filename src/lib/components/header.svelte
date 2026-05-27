b2809111c9a06fbf592d2b1f365003a085919518
Yellowinq <hig4342@naver.com>
2026-05-28 07:25:30 +0900

    :sparkles: feat(header): add header component with navigation and theme switcher

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { resolve } from '$app/paths';
+	import Search from './search.svelte';
+	import { Button } from './ui/button';
+	import ThemeSwitcher from './theme-switcher.svelte';
+	import I18nSwitcher from './i18n-switcher.svelte';
+	import ShieldUser from '@lucide/svelte/icons/shield-user';
+</script>
+
+<header class="h-16 border-b">
+	<div class="container mx-auto flex h-full items-center justify-between">
+		<div class="flex items-center">
+			<a href={resolve('/(app)')}>Medicine316</a>
+		</div>
+		<div class="flex items-center space-x-2">
+			<nav class="flex items-center space-x-4 p-4">
+				<a href={resolve('/(app)/faq')}>FAQ</a>
+			</nav>
+			<div class="w-64">
+				<Search />
+			</div>
+			<ThemeSwitcher />
+			<I18nSwitcher />
+			{#if page.data.hasSession}
+				<Button href={resolve('/admin')} variant="outline" size="icon">
+					<ShieldUser />
+				</Button>
+			{/if}
+		</div>
+	</div>
+</header>
