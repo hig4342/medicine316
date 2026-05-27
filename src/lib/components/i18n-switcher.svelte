524fc4d29350330a279f172b60589210cd35f4ec
Yellowinq <hig4342@naver.com>
2026-05-27 11:19:26 +0900

    :lipstick: feat(i18n): add language switcher component

---
+++ src/lib/components/i18n-switcher.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import { buttonVariants } from '$lib/components/ui/button';
+	import LanguagesIcon from '@lucide/svelte/icons/languages';
+	import { getLocale, setLocale, locales } from '$lib/paraglide/runtime';
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger class={buttonVariants({ variant: 'outline', size: 'icon' })}>
+		<LanguagesIcon />
+		<span class="sr-only">Switch language</span>
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content class="w-min">
+		<DropdownMenu.RadioGroup bind:value={getLocale, setLocale}>
+			{#each locales as locale (locale)}
+				{@const localeName = new Intl.DisplayNames([locale], { type: 'language' }).of(locale)}
+				<DropdownMenu.RadioItem value={locale}>
+					{localeName}
+				</DropdownMenu.RadioItem>
+			{/each}
+		</DropdownMenu.RadioGroup>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
